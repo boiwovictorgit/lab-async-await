3ecbf4d59d64a5f3e869d9c7976cf9819f66e3ea
Boiwo Kipkurui Benard <boiwo.victor@student.moringaschool.com>
2026-06-23 22:04:36 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await response.json();
+
+  const postList = document.querySelector("#post-list");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = data.title;
+
+  const p = document.createElement("p");
+  p.textContent = data.body;
+
+  postList.appendChild(h1);
+  postList.appendChild(p);
+}
+
+getData();
